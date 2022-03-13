20916674e85362f29ccaa3fc9925a237c274d407
dewonlupin <dewonlupin@gmail.com>
2022-03-12 20:46:01 -0800

    First commit in main.
    Only check main.cpp for project.

---
+++ cmake-build-debug/CMakeFiles/206_A3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "206_A3.exe"
+  "206_A3.exe.manifest"
+  "206_A3.pdb"
+  "CMakeFiles/206_A3.dir/main.cpp.obj"
+  "lib206_A3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/206_A3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
